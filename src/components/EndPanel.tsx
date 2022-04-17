5e9327a88c4f37e6d43efe46946aa595b780d8c0
David Mo <44306450+unknown@users.noreply.github.com>
2022-04-17 17:15:27 -0400

    Add a max width to panels

--- src/components/EndPanel.tsx
+++ src/components/EndPanel.tsx
@@ -5,9 +5,8 @@ interface EndPanelProps {
 
 const EndPanel = ({ onStart, score }: EndPanelProps) => {
   return (
-    <div className="h-screen flex flex-col items-center justify-start">
-      <div className="sm:basis-1/6 lg:basis-1/4" />
-      <div className="basis-1/3 flex flex-col w-full md:w-2/3 lg:w-5/12 p-6 md:shadow-xl rounded-lg">
+    <div className="h-screen flex flex-col items-center justify-start md:justify-center">
+      <div className="max-w-2xl h-80 flex flex-col w-full p-12 md:shadow-xl rounded-lg">
         <div className="flex items-center justify-between">
           <h1 className="text-2xl font-bold">Arithmetic</h1>
         </div>
--- src/components/ProblemPanel.tsx
+++ src/components/ProblemPanel.tsx
@@ -18,15 +18,14 @@ const ProblemPanel = ({
   value,
 }: ProblemPanelProps) => {
   return (
-    <div className="h-screen flex flex-col items-center justify-start">
-      <div className="sm:basis-1/6 lg:basis-1/4" />
-      <div className="basis-1/3 flex flex-col w-full md:w-2/3 lg:w-5/12 p-6 md:shadow-xl rounded-lg">
+    <div className="h-screen flex flex-col items-center justify-start md:justify-center">
+      <div className="max-w-2xl h-80 flex flex-col w-full p-12 md:shadow-xl rounded-lg">
         <div className="flex items-center justify-between">
           <h1 className="text-2xl">Seconds: {seconds}</h1>
           <p className="text-2xl">{score}</p>
         </div>
         <div className="flex flex-col h-full items-center">
-          <div className="py-4" />
+          <div className="py-2" />
           <p className="text-2xl">
             {problem?.num1} {problem?.operation} {problem?.num2} =
           </p>
